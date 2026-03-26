c38089c3389560807757ed7a1f2b20e45aed9ef9
sogir <sogir@ridwa.com>
2026-03-26 20:45:00 +0600

    add database core

--- app/Core/Database.php
+++ app/Core/Database.php
@@ -4,9 +4,27 @@ namespace App\Core;
 
 class Database
 {   
-     public static function connect()
+     protected $PDO;
+
+     public function __construct()
      {
-          
-     
+          $host = $_ENV['DB_HOST'] ?? 'localhost';
+          $db   = $_ENV['DB_NAME'] ?? '';
+          $user = $_ENV['DB_USER'] ?? 'root';
+          $pass = $_ENV['DB_PASS'] ?? '';
+
+          try {
+               $dsn = "mysql:host=$host;dbname=$db";
+               $this->PDO = new \PDO($dsn, $user, $pass);
+          }
+          catch (\PDOException $e)
+          {
+               echo $e->getMessage();
+          }
+     }
+
+     public function getConnection()
+     {
+          return $this->PDO;
      }
 }
--- public/index.php
+++ public/index.php
@@ -3,3 +3,9 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 require_once __DIR__ . '/../routes/web.php';
+
+
+$db = new \App\Core\Database();
+if ($db->getConnection()) {
+    echo "Database is connected!";
+}
